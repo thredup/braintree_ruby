eb5746ddabeb160909f1a73ffe6d01d55a1bea2d
Dan DeMeyere <dan@thredup.com>
2011-06-29 12:13:43 -0700

    [BUG] I have a feeling this might take a couple of commits

--- lib/braintree/http.rb
+++ lib/braintree/http.rb
@@ -70,6 +70,7 @@ module Braintree
         end
         response = http.request(request)
         @config.logger.info "[Braintree] [#{_current_time}] #{request.method} #{path} #{response.code}"
+        BRAINTREE_LOGGER_PRINT ||= true
         if BRAINTREE_LOGGER_PRINT
           @config.logger.debug "[Braintree] [#{_current_time}] #{response.code} #{response.message}"
         end
